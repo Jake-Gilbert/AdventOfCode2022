34a43d1d5a90c36ff7e1760f552a9bc70ee88574
Jake Gilbert <gilberj1@aston.ac.uk>
2022-12-09 10:47:53 +0000

    Advent of Code 2022 - Day One
    Iterates through a backsack of calory items.
    Finds the largest calory value as well as the
    value of the top three largest values,

---
+++ Day-1/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args)
+    {
+        final var caloryCalculator = new CaloryCalculator("AdventOfCode2022/Day-1/src/food_counts");
+    }
+}
